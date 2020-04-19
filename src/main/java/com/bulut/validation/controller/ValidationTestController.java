2ddda1d33eab411e118213c1d13091f0804a23c1
bulutcakan <bcakan@migros.com.tr>
2020-04-19 10:05:07 +0300

    big bang

---
+++ src/main/java/com/bulut/validation/controller/ValidationTestController.java
@@ -0,0 +1,36 @@
+package com.bulut.validation.controller;
+
+import com.bulut.validation.model.SampleDTO;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequestMapping("/test")
+@Validated
+public class ValidationTestController {
+
+    @PostMapping("/model")
+    public ResponseEntity testDTOValidation(@RequestBody SampleDTO sampleDTO) {
+        return null;
+
+    }
+
+    @PostMapping("/{name}")
+    public ResponseEntity testDTOValidation(@PathVariable @NotBlank String name,@RequestParam @Positive Long age,@RequestParam
+                                            @Range(min = 99, max = Long.MAX_VALUE,
+                                                    message = "Cok bu rakam cok") Long number) {
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+
+    }
+}
---
+++ src/main/java/com/bulut/validation/model/SampleDTO.java
@@ -0,0 +1,15 @@
+package com.bulut.validation.model;
+
+public class SampleDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private String adress;
+
+    private Integer age;
+
+}
